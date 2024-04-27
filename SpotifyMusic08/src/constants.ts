9dcdf3992794ea03805c1113cd8f9cbb9d8d0ac0
aman-somvanshi <amansomvanshi2002@gmail.com>
2024-04-27 21:53:22 +0530

    Usage of React Native Track Player package

---
+++ SpotifyMusic08/src/constants.ts
@@ -0,0 +1,67 @@
+import { Track } from "react-native-track-player";
+
+export const playListData : Track[] = [
+    {
+        id: 1,
+        title: 'Bekhayali (Arijit Singh Version)',
+        artist: 'Arijit Singh',
+        album: 'Kabir Singh',
+        artwork:
+          'https://i.scdn.co/image/ab67616d0000b27390d98409f70719daa0dc0421',
+        url: require('./assets/audio/Bekhayali - Arijit Singh Version.mp3'),
+      },
+      {
+        id: 2,
+        title: 'Photograph',
+        artist: 'Ed Sheeran',
+        album: 'x(Deluxe Edition)',
+        artwork:
+          'https://i.scdn.co/image/ab67616d0000b27313b3e37318a0c247b550bccd',
+        url: require('./assets/audio/Ed Sheeran - Photograph.mp3'),
+      },
+      {
+        id: 3,
+        title: 'Longing (Redemptions)',
+        artist: 'Gustavo Santaolalla',
+        album: 'The Last of Us Part II (Original Soundtrack)',
+        artwork:
+          'https://i.scdn.co/image/ab67616d0000b2736582b0de8c4acde5f02f4f51',
+        url: require('./assets/audio/Gustavo Santaolalla - Longing (Redemptions) (from The Last of Us Part II).mp3'),
+      },
+      {
+        id: 4,
+        title: 'I Wanna Be Yours',
+        artist: 'Arctic Monkeys',
+        album: 'AM',
+        artwork:
+          'https://i.scdn.co/image/ab67616d0000b2734ae1c4c5c45aabe565499163',
+        url: require('./assets/audio/I Wanna Be Yours.mp3'),
+      },
+      {
+        id: 5,
+        title: 'Future Days',
+        artist: 'Troy Baker',
+        album: 'The Last of Us Part II: Covers and Rarities',
+        artwork:
+          'https://i.scdn.co/image/ab67616d0000b273510bff47568e93aecbc3cb80',
+        url: require('./assets/audio/Joel - Future Days (from The Last of Us Part II).mp3'),
+      },
+      {
+        id: 6,
+        title: 'True Faith',
+        artist: 'Lotte Kestner',
+        album: 'Stolen',
+        artwork:
+          'https://i.scdn.co/image/ab67616d0000b273c810376ce0f8ee8a12420e4b',
+        url: require('./assets/audio/Lotte Kestner - True Faith  The Last of Us Part II.mp3'),
+      },
+      {
+        id: 7,
+        title: 'Love Is A Long Road',
+        artist: 'Tom Petty',
+        album: 'Full Moon Fever',
+        artwork:
+          'https://assetsio.gnwcdn.com/gta-6-main-art-01.png?width=1200&height=1200&fit=crop&quality=100&format=png&enable=upscale&auto=webp',
+        url: require('./assets/audio/Tom Petty - Love Is A Long Road.mp3'),
+      },
+]
