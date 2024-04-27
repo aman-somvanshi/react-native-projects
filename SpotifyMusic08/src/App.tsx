17f3198dad320aba5c71539012db4608dc642fb9
aman-somvanshi <amansomvanshi2002@gmail.com>
2024-04-27 22:07:12 +0530

    setting up the music Service

--- SpotifyMusic08/src/App.tsx
+++ SpotifyMusic08/src/App.tsx
@@ -1,14 +1,39 @@
 import React from 'react';
 import {
+  ActivityIndicator,
   SafeAreaView,
   StatusBar,
   StyleSheet,
   Text,
   View,
 } from 'react-native';
+import { useState, useEffect } from 'react';
+import { setupPlayer, addTrack } from '../musicPlayerServices';
 
 function App(): React.JSX.Element {
+  const [isPlayerReady, setIsPlayerReady] = useState(false)
 
+  async function setup(){
+    let isSetup = await setupPlayer()
+
+    if (isSetup) {
+      await addTrack()
+    }
+    setIsPlayerReady(isSetup)
+  }
+
+  useEffect(() => {
+    setup()
+  }, [])
+  
+  if (!isPlayerReady){
+    return(
+      <SafeAreaView>
+        <ActivityIndicator/>
+      </SafeAreaView>
+    )
+  }
+  
   return (
     <SafeAreaView >
       <StatusBar
@@ -20,7 +45,9 @@ function App(): React.JSX.Element {
 }
 
 const styles = StyleSheet.create({
-
+  container:{
+    flex:1,
+  },
 });
 
 export default App;
