186bbbb35c58e0eafa0659c008327ce6310590fb
aman-somvanshi <amansomvanshi2002@gmail.com>
2024-04-30 23:52:21 +0530

    10th app

---
+++ Shopping10/src/App.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+//Navigation
+import {NavigationContainer} from '@react-navigation/native'
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  )
+}
+
+export default App
